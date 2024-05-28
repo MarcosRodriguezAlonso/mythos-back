3feb237400c26036617d9d2a0e0b22e4b296816e
Marcos Rodríguez <e.marcosra@gmail.com>
2024-05-28 15:53:09 +0200

    Create CreatureStructure interface

---
+++ src/creatures/types.ts
@@ -0,0 +1,12 @@
+interface CreatureStructure {
+  _id: string;
+  name: string;
+  author: string;
+  lineage: string;
+  augury: boolean;
+  imageUrl: string;
+  alternativeText: string;
+  description: string;
+}
+
+export default CreatureStructure;
