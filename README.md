ece81bfff8ab5053506b2a1e407d93d767ac2133
Marcos <e.marcosra@gmail.com>
2024-06-02 12:09:07 +0200

    Create readme

---
+++ README.md
@@ -0,0 +1,12 @@
+**DOCUMENTACIÓN DE LA API**
+
+**Endpoint GET /creatures:**
+
+Descripción
+
+- Este endpoint devuelve un objeto con una propiedad creatures que apunta a un array de criaturas.
+
+Response
+
+- 200 OK: Si todo funcionacorrectamente se emite una response con un objeto con una propiedad creatures que apunta al array de criaturas.
+- 404 Not found: Se responderá así si el path es incorrecto.
--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,9 @@
 import "dotenv/config.js";
-import chalk from "chalk";
 import connectDataBase from "./database/index.js";
-import app from "./server/app.js";
+import { startServer } from "./server/app.js";
 
-const port = process.env.PORT ?? 6060;
 const dataBaseUrl = process.env.MONGODB_URL!;
 
 await connectDataBase(dataBaseUrl);
 
-app.listen(port, () => {
-  console.log(`Escuchando en ${chalk.blue(`http://localhost:${port}`)}`);
-});
+startServer();
