c36fc7461887c87f25cf709bba554053a5b8431e
Marcos <e.marcosra@gmail.com>
2024-06-04 19:53:21 +0200

    Add try catch to ensure the app only starts if the server is listening

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,15 @@
 import "dotenv/config.js";
 import connectDataBase from "./database/index.js";
 import { startServer } from "./server/app.js";
+import chalk from "chalk";
 
 const dataBaseUrl = process.env.MONGODB_URL!;
 
-await connectDataBase(dataBaseUrl);
+try {
+  await connectDataBase(dataBaseUrl);
+} catch (error) {
+  console.log(chalk.red("An error ocurred"));
+  process.exit();
+}
 
 startServer();
